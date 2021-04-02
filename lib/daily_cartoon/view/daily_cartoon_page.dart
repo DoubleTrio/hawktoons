655fcf53c330407fdb36b457b82605492b7b9812
DoubleTrio <lakacey03@gmail.com>
2021-04-02 12:43:04 -0400

    adjusted l10n daily cartoon tile

--- lib/daily_cartoon/view/daily_cartoon_page.dart
+++ lib/daily_cartoon/view/daily_cartoon_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:history_app/daily_cartoon/bloc/daily_cartoon.dart';
+import 'package:history_app/l10n/l10n.dart';
 import 'package:political_cartoon_repository/political_cartoon_repository.dart';
 
 class DailyCartoonPage extends StatelessWidget {
@@ -49,28 +50,34 @@ class _DailyCartoonViewState extends State<DailyCartoonView> {
 class PoliticalCartoonCardLoader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final l10n = context.l10n;
     return BlocBuilder<DailyCartoonBloc, DailyCartoonState>(
         builder: (context, state) {
       if (state is DailyCartoonInProgress) {
         return const CircularProgressIndicator();
       }
       if (state is DailyCartoonLoad) {
-        return Container(
-          key: const Key('dailyCartoonView_DailyCartoonLoad_card'),
-          width: 200,
-          height: 200,
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                  offset: Offset(0, 4),
-                  blurRadius: 5,
-                  spreadRadius: 3,
-                  color: Colors.black54)
-            ],
-          ),
-          child: Text('${state.dailyCartoon.description}'),
+        return Column(
+          children: [
+            Text(l10n.dailyCartoonTitle),
+            Container(
+              key: const Key('dailyCartoonView_DailyCartoonLoad_card'),
+              width: 200,
+              height: 200,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 4),
+                      blurRadius: 5,
+                      spreadRadius: 3,
+                      color: Colors.black54)
+                ],
+              ),
+              child: Text('${state.dailyCartoon.description}'),
+            ),
+          ],
         );
       } else {
         return const Text('Error while fetching political cartoon',
